b63d689c4608e5f6a193a902e05d76d965ee0a57
Adrian Oprea <adrian@codesi.nz>
2016-05-13 07:30:26 +0300

    Initial version
    Signed-off-by: Adrian Oprea <adrian@codesi.nz>

---
+++ handlers/translate_message.go
@@ -0,0 +1,33 @@
+package handlers
+
+import (
+	"github.com/ant0ine/go-json-rest/rest"
+	"net/http"
+	"strings"
+)
+
+type Message struct {
+	Locale string
+	Text   string
+}
+
+func MaskSensitiveData(w rest.ResponseWriter, r *rest.Request) {
+	message := Message{}
+	err := r.DecodeJsonPayload(&message)
+
+	if err != nil {
+		rest.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	processed_message := mask_sensitive_data(message.Text)
+	w.WriteJson(
+		&Message{
+			Locale: message.Locale,
+			Text:   processed_message,
+		},
+	)
+}
+
+func mask_sensitive_data(s string) string {
+	return strings.ToUpper(s)
+}
---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"./handlers"
+	// "./persistence"
+	"github.com/ant0ine/go-json-rest/rest"
+	"log"
+	"net/http"
+)
+
+func main() {
+	api := rest.NewApi()
+	api.Use(rest.DefaultDevStack...)
+	api.Use(&rest.AuthBasicMiddleware{
+		Realm: "test zone",
+		Authenticator: func(userId string, password string) bool {
+			if userId == "admin" && password == "admin" {
+				return true
+			}
+			return false
+		},
+	})
+	router, err := rest.MakeRouter(
+		rest.Post("/mask", handlers.MaskSensitiveData),
+	)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	api.SetApp(router)
+
+	log.Println("Server running on port 8080")
+	log.Fatal(http.ListenAndServe(":8080", api.MakeHandler()))
+}
