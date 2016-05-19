84b7060eca30140270cb5df9cd0283f8314ffb96
Adrian Oprea <adrian@codesi.nz>
2016-05-19 19:45:58 +0300

    test: Update testing script to include more data
    Signed-off-by: Adrian Oprea <adrian@codesi.nz>

--- api_test.sh
+++ api_test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+REQUEST_BODY='{"Locale": "en_US","Text": "This should be masked as it is a credit card 4111111111111111(VISA). This too should be masked as it is a North American phone number 1-(555)-555-5555? The service can also mask Social Security Numbers like this one: 555-55-5555"}'
 for ((i = 1; i <= 1000; i++)); do
-  curl -XPOST -H "Content-Type: application/json" -H "Accept:application/json" -H "Authorization: Basic YWRtaW46YWRtaW4=" -d '{"Locale": "en_US", "Text": "This is some text that 4023600421224531 to be masked. And more to come de 7123777722227431 ce? This is some text that 4023600421224531 to be masked. And more to come de 7123777722227431 ce?"}' http://localhost:8080/mask
+  curl -XPOST -H "Content-Type: application/json" -H "Accept:application/json" -H "Authorization: Basic YWRtaW46YWRtaW4=" -d "${REQUEST_BODY}" http://localhost:8080/mask
 done
