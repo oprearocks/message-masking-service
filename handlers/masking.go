6af9782d8040ad48d6b7c55ecf137ad4c2bd3014
Adrian Oprea <adrian@codesi.nz>
2016-05-19 23:25:56 +0300

    feat: Enable the user to add their custom masking
    - The user can either specify their custom masking symbol or a whole
    word to be placed instead of the data being replaced
    Request sample:
    ```
    {
        "Locale": "en_US",
        "Text": "This should be masked as it is a credit card 4111111111111111(VISA). This too should be masked as it is a North American phone number 1-(555)-555-5555? The service can also mask Social Security Numbers like this one: 555-55-5555",
        "MaskSymbol": "(hidden)"
    }
    ```
    
    Signed-off-by: Adrian Oprea <adrian@codesi.nz>

--- handlers/masking.go
+++ handlers/masking.go
@@ -8,45 +8,58 @@ import (
 	"github.com/ant0ine/go-json-rest/rest"
 )
 
+// Message is a code representation of the data sent by the API user through the wire
 type Message struct {
-	Locale string
-	Text   string
+	Locale     string
+	Text       string
+	MaskSymbol string
 }
 
+// MaskSensitiveData is the route handler that responds whenever the `/mask` route
+// has been called with valid data
 func MaskSensitiveData(w rest.ResponseWriter, r *rest.Request) {
 
 	message := Message{}
 	err := r.DecodeJsonPayload(&message)
+	var maskSymbol = "X"
+
+	if message.MaskSymbol != "" {
+		maskSymbol = message.MaskSymbol
+	}
 
 	if err != nil {
 		rest.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	processed_message := maskSensitiveData(message.Text, persistence.Expressions)
+	processedMessage := maskSensitiveData(message.Text, persistence.Expressions, maskSymbol)
 	w.WriteJson(
 		&Message{
 			Locale: message.Locale,
-			Text:   processed_message,
+			Text:   processedMessage,
 		},
 	)
 }
 
-func maskSensitiveData(s string, expressionMap map[string]string) string {
+func maskSensitiveData(s string, expressionMap map[string]string, maskSymbol string) string {
 	for _, value := range expressionMap {
-		s = applyExpression(s, value)
+		s = applyExpression(s, value, maskSymbol)
 	}
 
 	return s
 }
 
-func applyExpression(s string, expression string) string {
+func applyExpression(s string, expression string, maskSymbol string) string {
 	re := regexp.MustCompile(expression)
 	return re.ReplaceAllStringFunc(s, func(str string) string {
-		var mask string
-		for range str {
-			mask += "X"
+		var maskedValue string
+		if len(maskSymbol) > 1 {
+			return maskSymbol
 		}
-		return mask
+
+		for range str {
+			maskedValue += maskSymbol
+		}
+		return maskedValue
 	})
 }
